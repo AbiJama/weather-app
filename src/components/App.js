3690079716f0ed1e9ea7b64cf53d216634a70882
Abi Jama <abi_jama88@hotmail.com>
2024-03-04 17:44:37 +0000

    created a search form to get Forecast from specific city

--- src/components/App.js
+++ src/components/App.js
@@ -1,14 +1,16 @@
-import React, { useState, useEffect } from "react";
+import { React, useState, useEffect } from "react";
 import "../styles/App.css";
 import LocationDetails from "./LocationDetails";
 import ForecastSummaries from "./ForecastSummaries";
 import ForecastDetails from "./ForecastDetails";
 import getForecast from "../requests/getForecast";
+import SearchForm from "./SearchForm";
 
 function App() {
   const [forecasts, setForecasts] = useState([]);
   const [location, setLocation] = useState({ city: "", country: "" });
   const [selectedDate, setSelectedDate] = useState(0);
+  const [searchText, setSearchText] = useState("");
 
   const selectedForecast = forecasts.find(
     (forecast) => forecast.date === selectedDate,
@@ -18,6 +20,16 @@ function App() {
     setSelectedDate(date);
   };
 
+  const handleCitySearch = () => {
+    getForecast(
+      searchText,
+      setSearchText,
+      setSelectedDate,
+      setForecasts,
+      setLocation,
+    );
+  };
+
   useEffect(() => {
     getForecast(setSelectedDate, setForecasts, setLocation);
   }, []);
@@ -25,6 +37,12 @@ function App() {
   return (
     <div className="weather-app">
       <LocationDetails city={location.city} country={location.country} />
+      <SearchForm
+        searchText={searchText}
+        setSearchText={setSearchText}
+        onSubmit={handleCitySearch}
+      />
+
       <ForecastSummaries
         forecasts={forecasts}
         onForecastSelect={handleForecastSelect}
---
+++ src/tests/components/App.test.js
@@ -0,0 +1,10 @@
+import { render, screen } from "@testing-library/react";
+import React from "react";
+import App from "../../components/App";
+import forecast from "../../data/forecast.json";
+
+describe("App", () => {
+  render(<App location={forecast.location} forecasts={forecast.forecasts} />);
+  const h1Element = screen.getByText(/Manchester, UK/i);
+  expect(h1Element).toBeInTheDocument();
+});
